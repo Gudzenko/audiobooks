fd7778537910479f17cf94f148ad2d440f5a5f02
Gudzenko <o.gudzenko@weegree.com>
2024-11-02 12:43:23 +0100

    config new project

---
+++ audiobooks/authapp/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
+
+urlpatterns = [
+    path('', views.ProfileView.as_view(), name='profile'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('manage-users/', views.ManageUsersView.as_view(), name='manage_users'),
+    path('admin-manage-users/', views.AdminManageUsersView.as_view(), name='admin_manage_users'),
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='authapp/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='authapp/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authapp/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='authapp/password_reset_complete.html'), name='password_reset_complete'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
